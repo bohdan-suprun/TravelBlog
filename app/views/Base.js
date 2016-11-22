d44bbad37336f942d451373ccc28448557b4beb9
Bohdan Suprun <bohdan_suprun@epam.com>
2016-11-22 13:06:54 +0200

    TB-32. Create prototype for child views

--- app/views/Base.js
+++ app/views/Base.js
@@ -1,4 +1,23 @@
+// Factory method (Constructor)
 module.exports = function(response, template) {
 	this.response = response;
 	this.template = template;
 };
+
+module.exports.prototype = {
+	extend: function(properties) {
+		// Deep copy of parent's properties
+		var Child = module.exports;
+		Child.prototype = module.exports.prototype;
+		for(var key in properties) {
+			Child.prototype[key] = properties[key];
+		}
+		return Child;
+	},
+	render: function(data) {
+		// Template render
+		if(this.response && this.template) {
+			this.response.render(this.template, data);
+		}
+	}
+}
